03e52cbd589e90bf5b36f69a4dc2877b00684a1f
ks <koushuang999@163.com>
2019-11-08 15:09:38 +0800

    feat: add file

--- data/data.js
+++
@@ -1,1 +0,0 @@
-export default {data : {"share":{"total":5,"pageSize":5,"totalPage":1,"files":["blog1","blog2","blog3","blog4","pdf"]},"weekly":{"total":8,"pageSize":5,"totalPage":2,"files":["week1","week2","week3","week4","week5","week6","week7","week8"]}}}
